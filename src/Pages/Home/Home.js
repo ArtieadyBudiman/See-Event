9c87302e4ded7a4c354d915d06525a5f746e1d52
Hamdani Abdullah <hamdaniadullah961106@gmail.com>
2022-01-21 22:04:56 +0700

    testing

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,30 @@
+import bgImg from "./Assets/img/banner.png";
+import Search from "../../Components/ShareComponent/Search";
+import styles from "./index.module.css";
+
+export default function Home() {
+  return (
+    <div
+      className={`container-fluid ${styles.banner}`}
+      style={{ backgroundImage: `url(${bgImg})` }}
+    >
+      <div className={`container ${styles.banner_content}`}>
+        <div
+          className="textContent me-auto"
+          style={{ width: "496px", height: "247px" }}
+        >
+          <h1 className={styles.title}>
+            <span style={{ color: "#F2D555" }}>Create </span>or
+            <span style={{ color: "#F2D555" }}> Find</span> <br />
+            interesting
+            <br />
+            <span style={{ color: "#F2D555" }}>Events</span> around
+            <br />
+            The world
+          </h1>
+        </div>
+        <Search />
+      </div>
+    </div>
+  );
+}
